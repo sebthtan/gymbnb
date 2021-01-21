3bf3d7167c31ed20dbc6a2dee10fdb6269417a02
Sebastian Tan <sebthtan@yahoo.com>
2021-01-21 17:11:41 -0600

    Error handling

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 const routes = require('./routes');
+const { ValidationError } = require('sequelize');
 
 const app = express();
 
@@ -38,6 +39,33 @@ app.use(
 app.use(routes)
 
 
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
 
+
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack,
+    });
+});
 
 module.exports = app

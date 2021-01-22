d12feeb78accb1cdcb9dbfc262d689f23e5fa870
Sebastian Tan <sebthtan@yahoo.com>
2021-01-22 12:27:20 -0600

    react-redux setup

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
-const asyncHandler = require('express-async-handler')
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-const { User } = require('../../db/models')
 
 router.use('/session', sessionRouter);
 

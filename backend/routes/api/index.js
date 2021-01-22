d6db7eae9dfc81db2e3efbd4297dae604f6df734
Sebastian Tan <sebthtan@yahoo.com>
2021-01-21 23:06:19 -0600

    api complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -8,4 +8,8 @@ router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
+router.post('/test', function (req, res) {
+    res.json({ requestBody: req.body });
+});
+
 module.exports = router;

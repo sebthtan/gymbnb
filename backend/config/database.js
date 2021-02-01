1b0a928d20438ba434d15bcaa5c9d1fb4b53606b
Sebastian Tan <sebthtan@yahoo.com>
2021-01-31 20:10:34 -0600

    prod

--- backend/config/database.js
+++ backend/config/database.js
@@ -18,6 +18,11 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    },
     seederStorage: 'sequelize',
   },
 };

1784954b1b52cfb211c1c6783d9e0ed56e37c3fe
Sebastian Tan <sebthtan@yahoo.com>
2021-01-31 19:59:18 -0600

    package

--- backend/config/database.js
+++ backend/config/database.js
@@ -16,7 +16,7 @@ module.exports = {
     seederStorage: "sequelize",
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'DATABASE_URL' + 'sslmode=require',
     dialect: 'postgres',
     seederStorage: 'sequelize',
   },
